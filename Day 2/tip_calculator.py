0d9fe24e318a78cb4f6be8aacda5c7517d05af8d
grellheist <grellheist@tutanota.com>
2022-12-21 15:45:47 -0300

    Updated

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -1,9 +1,6 @@
 print("Welcome to the tip calculator!")
 bill = input("What was the total bill? $")
-print(bill)
 split = input("How many people to split the bill? ")
-print(split)
 percentage = input("What percentage tip would you like to give? 10, 12 or 15? ")
-print(percentage)
 value_per_person = #implement
 print(f"Each person should pay: ${value_per_person}")
