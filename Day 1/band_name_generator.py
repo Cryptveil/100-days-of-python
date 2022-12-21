7c4e274aca265a6858127c6adfd5615fa08409f2
Grellheist <hjgameplays@hotmail.com>
2022-12-21 11:48:47 -0300

    Updated

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Hello and welcome to the program!")
+city = input("What city did you grow up in? ")
+pet = input("Gimme a pet name: ")
+print(f"Your band name could totaly be {city} {pet}")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 100-days-of-python
+
+This repository will contain 100 Python projects, developed during the 100 Days of Code: The Complete Python Pro Bootcamp for 2023 course on Udemy.
