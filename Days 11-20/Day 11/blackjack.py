6198c4a22cd2e9167f59b91ce268f82490659025
Grellheist <grellheist@tutanota.com>
2022-12-29 16:24:12 -0300

    Updated

--- Days 11-20/Day 11/blackjack.py
+++ Days 11-20/Day 11/blackjack.py
@@ -1,29 +1,3 @@
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
 import random
 from art import logo
 import os
@@ -50,7 +24,7 @@ player_hand = []
 computer_hand = []
 state = ["You win 😃", "You lose 😤"]
 game_is_running = True
-
+game_over = False
 
 while game_is_running:
     more_cards = True
@@ -63,9 +37,9 @@ while game_is_running:
         # Gets the player score (sum of all the cards on hand)
         player_score = calculate_score(player_hand)
         computer_score = calculate_score(player_hand)
-        if player_score == 0:
-            print(state[[0]])
-
+        # Checks if the game should be over
+        if player_score == 0 or computer_score == 0 or player_score > 21:
+            game_over = True
 
         # Clears the terminal for better user experience
         os.system("cls" if os.name == "nt" else "clear")
