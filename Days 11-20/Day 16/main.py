02b1b0eebedb55b17e301b2c95f454f917599075
grellheist <grellheist@tutanota.com>
2023-01-03 19:19:38 -0300

    Updated

--- Days 11-20/Day 16/main.py
+++ Days 11-20/Day 16/main.py
@@ -1,3 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+machine_is_running = True
+
+while machine_is_running:
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
+    if choice == "off":
+        machine_is_running = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            money_machine.make_payment(drink.cost)
+            coffee_maker.make_coffee(drink)
+        else:
+            pass
