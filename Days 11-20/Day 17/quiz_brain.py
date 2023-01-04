3354856323ee5c158d61c56f27fa9793272280ae
grellheist <grellheist@tutanota.com>
2023-01-04 12:11:38 -0300

    Updated

--- Days 11-20/Day 17/quiz_brain.py
+++ Days 11-20/Day 17/quiz_brain.py
@@ -3,6 +3,7 @@ class QuizBrain():
     def __init__(self, input):
         self.question_number = 0
         self.question_list = input
+        self.score = 0
 
 
     def still_has_questions(self):
@@ -14,3 +15,15 @@ class QuizBrain():
         self.question_number += 1
         question_text = input(f"Q.{self.question_number}: "
                               f"{current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
+
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {score}/{self.question_number}")
+
