3347f16e3a087be28eebac7cc5ffbd4315d0059e
grellheist <grellheist@tutanota.com>
2023-01-04 12:32:55 -0300

    Updated

--- Days 11-20/Day 17/data.py
+++ Days 11-20/Day 17/data.py
@@ -1,22 +1,439 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.",
-     "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football"
-     "pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with"
-     "your car, you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament,"
-     "you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer":
-     "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer":
-     "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7"
-     "times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer":
-     "True"}
-]
+    {
+        'category': 'Geography',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'A group of islands is called an &#039;archipelago&#039;.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Geography',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'Vatican City is a country.',
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Musicals & Theatres',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'In the play Oedipus Rex, Oedipus kills his father due to jealousy in loving his mother.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Science: Mathematics',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'The proof for the Chinese Remainder Theorem used in Number Theory was NOT developed by its first publisher, Sun Tzu.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'History',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Sargon II, a king of the Neo-Assyrian Empire, was a direct descendant of Sargon of Akkad.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Science & Nature',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'A defibrillator is used to start up a heartbeat once a heart has stopped beating.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'The Ace Attorney trilogy was suppose to end with &quot;Phoenix Wright: Ace Attorney &minus; Trials and Tribulations&quot; as its final game.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Japanese Anime & Manga',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'Studio Ghibli is a Japanese animation studio responsible for the films &quot;Wolf Children&quot; and &quot;The Boy and the Beast&quot;.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': '&quot;Half-Life 2&quot; runs on the Source Engine.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Science: Gadgets',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'The communication protocol NFC stands for Near-Field Control.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Tony Hawk&#039;s Pro Skater was released in 1999.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Sports',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Tennis was once known as Racquetball.',
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Science & Nature',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'An atom contains a nucleus.',
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'History',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'In 1967, a magazine published a story about extracting hallucinogenic chemicals from bananas to raise moral questions about banning drugs.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Japanese Anime & Manga',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': "Gosho Aoyama Made This Series: (Detective Conan \/ Case Closed!)"
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Japanese Anime & Manga',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the nickname Kyon.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Animals',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'A flock of crows is known as a homicide.',
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'General Knowledge',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'The French word for &quot;glass&quot; is &quot;glace&quot;.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Music',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'A Saxophone is a brass instrument.',
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Vehicles',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Bugatti was an Italian car manufacturer.',
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Animals',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'A caterpillar has more muscles than humans do.',
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'In &quot;Undertale&quot;, the main character of the game is Sans.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'In Riot Games &quot;League of Legends&quot; the name of Halloween event is called &quot;The Reckoning&quot;.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Cartoon & Animations',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Nickelodeon rejected the pilot to Adventure Time.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'David Baszucki was a co-founder of ROBLOX Corporation.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Politics',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'The State of Queensland, Australia voted NO to a referendum for daylight savings in 1992.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Politics',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'Donald Trump won the popular vote in the 2016 United States presidential election.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Science: Mathematics',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'A &#039;Millinillion&#039; is a real number.',
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'Watch_Dogs 2 is a prequel.',
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'History',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'Kublai Khan is the grandchild of Genghis Khan?',
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Resident Evil 4 was originally meant to be a Nintendo GameCube exclusive.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'In Portal, the Companion Cube&#039;s ARE sentient.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Mythology',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'The Roman god &quot;Jupiter&quot; was first known as &quot;Zeus&quot; to the Greeks.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Celebrities',
+        'type': 'boolean',
+        'difficulty': 'hard',
+        'question': 'Lady Gaga&#039;s real name is Stefani Joanne Angelina Germanotta.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Science & Nature',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'An average human can go two weeks without water.',
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Science: Mathematics',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'The set of all algebraic numbers is countable.',
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'General Knowledge',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': '&quot;Santa Claus&quot; is a variety of melon.',
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Music',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Nick Mason is the only member to appear on every Pink Floyd album.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Japanese Anime & Manga',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'In the &quot;To Love-Ru&quot; series, Golden Darkness is sent to kill Lala Deviluke.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'The ghosts in &quot;Pac-Man&quot; and &quot;Ms. Pac-Man&quot; have completely different behavior.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Animals',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Finnish Lapphund dogs were used for herding reindeer.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'General Knowledge',
+        'type': 'boolean',
+        'difficulty': 'hard',
+        'question': 'Spoon theory is a theory, utilizing &quot;Spoons&quot; as a metaphor for energy they can use in a day.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'History',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'The French Kingdom helped the United States gain their independence over Great Britain during the Revolutionary War.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'In the &quot;S.T.A.L.K.E.R.&quot; series, the Freedom faction wishes to destroy the supernatural area known as  &quot;the Zone&quot;.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Cartoon & Animations',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Night on Bald Mountain was one of the musical pieces featured in Disney&#039;s 1940&#039;s film Fantasia.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Science: Mathematics',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'An isosceles triangle has two sides of equal length as opposed to three.'
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Science: Mathematics',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'A scalene triangle has two sides of equal length.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Entertainment: Music',
+        'type': 'boolean',
+        'difficulty': 'medium',
+        'question': 'Metallica collaborated with Rowan Atkinson&#039;s Mr Bean on a 1992 comic relief single.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    {
+        'category': 'Geography',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': "There is an island in Japan called \u014ckunoshima, A.K.A. &quot;Rabbit Island&quot;, so named because of it&#039;s huge population of rabbits."
+            ,
+        'correct_answer': 'True',
+        'incorrect_answers': ['False'],
+        },
+    {
+        'category': 'Entertainment: Video Games',
+        'type': 'boolean',
+        'difficulty': 'easy',
+        'question': 'The game &quot;Jetpack Joyride&quot; was created by &quot;Redbrick Studios&quot;.'
+            ,
+        'correct_answer': 'False',
+        'incorrect_answers': ['True'],
+        },
+    ]
