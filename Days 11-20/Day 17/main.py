819a710535e14504fbc3046148dc87e49746d682
grellheist <grellheist@tutanota.com>
2023-01-04 11:43:53 -0300

    Updated

--- Days 11-20/Day 17/main.py
+++ Days 11-20/Day 17/main.py
@@ -2,7 +2,7 @@ from question_model import Question
 from data import question_data
 
 question_bank = []
-for item in question_data:
+for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
