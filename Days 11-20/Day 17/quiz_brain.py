12f76c39ee1f4bd61cfdcdbe67eba923d311cb1b
grellheist <grellheist@tutanota.com>
2023-01-04 11:57:09 -0300

    Updated

--- Days 11-20/Day 17/quiz_brain.py
+++ Days 11-20/Day 17/quiz_brain.py
@@ -6,5 +6,6 @@ class QuizBrain():
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        question_text = input(f"Q.{self.question_number+1}:"
+        self.question_number += 1
+        question_text = input(f"Q.{self.question_number}: "
                               f"{current_question.text} (True/False)?: ")
