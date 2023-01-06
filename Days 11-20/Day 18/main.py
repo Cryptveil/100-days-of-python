d266ea5a0ce293465e5913548701cd0e107d0e56
grellheist <grellheist@tutanota.com>
2023-01-05 21:02:06 -0300

    Updated

--- Days 11-20/Day 18/main.py
+++ Days 11-20/Day 18/main.py
@@ -1,7 +1,12 @@
 import turtle
 import colorgram as cg
 
+rgb_colors = []
 color_list = cg.extract("image.jpg", 6)
 
-print(color_list)
-
+for i in range(len(color_list)):
+    r = color_list.rgb.r
+    g = color_list.rgb.g
+    b = color_list.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
