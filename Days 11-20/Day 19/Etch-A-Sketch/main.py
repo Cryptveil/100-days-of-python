ad9b0ed9bb20b5e9327399a70a21769d92919373
grellheist <grellheist@tutanota.com>
2023-01-06 20:59:27 -0300

    Updated

--- Days 11-20/Day 19/Etch-A-Sketch/main.py
+++ Days 11-20/Day 19/Etch-A-Sketch/main.py
@@ -12,7 +12,12 @@ def move_backwards():
     tim.backward(10)
 
 
+def move_clockwise():
+    tim.right(10)
+
+
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=move_clockwise)
 screen.exitonclick()
