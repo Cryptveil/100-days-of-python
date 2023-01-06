d94fecaf243915cde317aaddd6cedefb8c9b9a80
grellheist <grellheist@tutanota.com>
2023-01-05 21:02:47 -0300

    Updated

--- Days 11-20/Day 18/main.py
+++ Days 11-20/Day 18/main.py
@@ -4,9 +4,9 @@ import colorgram as cg
 rgb_colors = []
 color_list = cg.extract("image.jpg", 6)
 
-for i in range(len(color_list)):
-    r = color_list.rgb.r
-    g = color_list.rgb.g
-    b = color_list.rgb.b
+for color in color_list:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
     new_color = (r, g, b)
     rgb_colors.append(new_color)
