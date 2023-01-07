88704408ced1d0849e6f3057de6e6b6bf48c2953
grellheist <grellheist@tutanota.com>
2023-01-07 14:18:35 -0300

    Updated

--- Days 11-20/Day 20/snake.py
+++ Days 11-20/Day 20/snake.py
@@ -12,7 +12,6 @@ class Snake():
 
 
     def create_snake(self):
-
         for position in STARTING_POSITIONS:
             new_segment = Turtle(shape="square")
             new_segment.penup()
@@ -28,3 +27,27 @@ class Snake():
             self.snake_size[segment_number].goto(new_x, new_y)
         self.snake_size[0].forward(MOVE_DISTANCE)
 
+
+    def up(self):
+        CURRENT_POSITION = self.snake_size[0].heading()
+        if CURRENT_POSITION == 0 or CURRENT_POSITION == 180:
+            self.snake_size[0].setheading(90)
+
+
+    def down(self):
+        CURRENT_POSITION = self.snake_size[0].heading()
+        if CURRENT_POSITION == 0 or CURRENT_POSITION == 180:
+            self.snake_size[0].setheading(270)
+
+
+    def left(self):
+        CURRENT_POSITION = self.snake_size[0].heading()
+        if CURRENT_POSITION == 90 or CURRENT_POSITION == 270:
+            self.snake_size[0].setheading(180)
+
+
+    def right(self):
+        CURRENT_POSITION = self.snake_size[0].heading()
+        if CURRENT_POSITION == 90 or CURRENT_POSITION == 270:
+            self.snake_size[0].setheading(0)
+ 
