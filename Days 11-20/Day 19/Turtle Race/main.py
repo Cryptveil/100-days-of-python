b2375e0bf12b344c95a9f7e9e572034a0783b589
grellheist <grellheist@tutanota.com>
2023-01-07 15:06:59 -0300

    Updated

--- Days 11-20/Day 19/Turtle Race/main.py
+++ Days 11-20/Day 19/Turtle Race/main.py
@@ -4,7 +4,7 @@ import random
 is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will"
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will "
                             "win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 turtle_list = []
