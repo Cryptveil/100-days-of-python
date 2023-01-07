13de8944f99833bad8a3104f458a9273440ad558
grellheist <grellheist@tutanota.com>
2023-01-07 13:25:38 -0300

    Updated

--- Days 11-20/Day 20/main.py
+++ Days 11-20/Day 20/main.py
@@ -4,6 +4,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+snake_size = []
+position = 0
+
+for segments in range(0, 3):
+    new_segment = Turtle(shape="square")
+    new_segment.penup()
+    new_segment.color("white")
+    new_segment.goto(x=position, y=0)
+    snake_size.append(new_segment)
+    position -= 20
 
 
 screen.exitonclick()
