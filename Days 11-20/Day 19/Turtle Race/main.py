5d19fc1a207defe6776150ffc3475900d9c59e9f
grellheist <grellheist@tutanota.com>
2023-01-06 21:23:27 -0300

    Updated

--- Days 11-20/Day 19/Turtle Race/main.py
+++ Days 11-20/Day 19/Turtle Race/main.py
@@ -1,4 +1,10 @@
 from turtle import Turtle, Screen
 import random
 
+tim = Tutle()
+screen = Screen()
+screen.setup(width=500, height=400)
+screen.textinput(title="Make your bet", prompt="Which turtle will win the race?"
+                 "Enter a color: ")
 
+screen.exitonclick()
