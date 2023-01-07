80bf7481363e5488d5ae3bc25084b4501978db97
grellheist <grellheist@tutanota.com>
2023-01-06 21:34:54 -0300

    Updated

--- Days 11-20/Day 19/Turtle Race/main.py
+++ Days 11-20/Day 19/Turtle Race/main.py
@@ -1,10 +1,18 @@
 from turtle import Turtle, Screen
 import random
 
-tim = Tutle()
 screen = Screen()
 screen.setup(width=500, height=400)
-screen.textinput(title="Make your bet", prompt="Which turtle will win the race?"
-                 "Enter a color: ")
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will"
+                            "win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_pos = -100
 
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_pos)
+    y_pos += 30
+    
 screen.exitonclick()
