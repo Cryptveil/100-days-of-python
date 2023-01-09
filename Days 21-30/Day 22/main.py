e788a5211cdd95b4621f94a6bcfa83587c7ebb07
Grellheist <grellheist@tutanota.com>
2023-01-09 13:32:33 -0300

    Updated

--- Days 21-30/Day 22/main.py
+++ Days 21-30/Day 22/main.py
@@ -1,15 +1,21 @@
 from turtle import Screen
 from paddle import Paddle
 
+# Creates the screen
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600, startx=250, starty=250)
 screen.title("Pong")
-screen.tracer(0)
-screen.update
-screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
+screen.tracer(0)  # Stops the animation
+
+paddle2 = Paddle()  # Creates the paddle
+
+screen.listen()  # Listens to input
+screen.onkey(paddle2.up, "Up")
+screen.onkey(paddle2.down, "Down")
+game_is_on = True
+while game_is_on:
+    screen.update()  # Makes the animation runs again
 
 
 screen.exitonclick()
