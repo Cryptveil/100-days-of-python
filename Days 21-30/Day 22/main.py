615004131c0d5f46951c15fd56a7544dcff1e07d
Grellheist <grellheist@tutanota.com>
2023-01-09 13:11:06 -0300

    Updated

---
+++ Days 21-30/Day 22/main.py
@@ -0,0 +1,11 @@
+from turtle import Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600, startx=250, starty=250)
+screen.title("Pong")
+screen.tracer(0)
+screen.update
+
+
+screen.exitonclick()
