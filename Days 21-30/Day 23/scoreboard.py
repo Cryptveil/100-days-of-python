59f1c8af4a9ec6fb0c9894becae2e0806690bf72
Grellheist <grellheist@tutanota.com>
2023-01-10 11:09:02 -0300

    Updated

--- Days 21-30/Day 23/scoreboard.py
+++ Days 21-30/Day 23/scoreboard.py
@@ -1,5 +1,25 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
+ALIGNMENT = "left"
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.color("black")
+        self.goto(-250, 250)
+        self.update_scoreboard()
+        
+
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align=ALIGNMENT, font=FONT)
+        
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER.", align="center", font=FONT)
+
