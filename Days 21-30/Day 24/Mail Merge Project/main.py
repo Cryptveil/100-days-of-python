9b92a20696c2d48da5266bd1d8a36a9f7fb58cde
Grellheist <grellheist@tutanota.com>
2023-01-11 12:36:20 -0300

    Updated

--- Days 21-30/Day 24/Mail Merge Project/main.py
+++ Days 21-30/Day 24/Mail Merge Project/main.py
@@ -6,3 +6,15 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Names/invited_names.txt") as names:
+    name_list = names.readlines()
+    
+with open("./Input/Letters/starting_letter.txt") as letter:
+    letter_content = letter.read()
+    for name in name_list:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace("[name]", stripped_name)
+
+        print(new_letter)
+
