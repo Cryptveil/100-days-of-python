96e2e542728b3ae0ed4822e1a25e6f6ab471e44e
Grellheist <grellheist@tutanota.com>
2023-01-12 14:55:28 -0300

    Updated

--- Days 21-30/Day 25/My version/main.py
+++ Days 21-30/Day 25/My version/main.py
@@ -14,10 +14,10 @@ guessed_states = []
 
 while len(guessed_states) < 27:
     answer_state = screen.textinput(
-        title=f"{len(guessed_states)}/{len(state_list)} States Correct",
-        prompt="What's another state's name?").title()
+        title=f"{len(guessed_states)}/{len(state_list)} Estados Corretos",
+        prompt="Qual o próximo estado?").title()
 
-    if answer_state == "Exit":
+    if answer_state == "Sair":
         missing_states = []
         for state in state_list:
             if state not in guessed_states:
