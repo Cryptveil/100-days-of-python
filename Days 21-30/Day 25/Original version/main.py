fe0b9f039de84b4d7eb1e275a231b08aa19d713d
Grellheist <grellheist@tutanota.com>
2023-01-12 15:08:06 -0300

    Updated

--- Days 21-30/Day 25/Original version/main.py
+++ Days 21-30/Day 25/Original version/main.py
@@ -8,15 +8,18 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# Opens the csv containing the states name
 data = pd.read_csv("50_states.csv")
 state_list = data.state.tolist()
 guessed_states = []
 
+# Main loop of the game
 while len(guessed_states) < 50:
     answer_state = screen.textinput(
         title=f"{len(guessed_states)}/{len(state_list)} States Correct",
         prompt="What's another state's name?").title()
 
+    # Puts the missing states on another csv to review later
     if answer_state == "Exit":
         missing_states = []
         for state in state_list:
@@ -29,7 +32,9 @@ while len(guessed_states) < 50:
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
+        # Gets the states data comparing it to the input state
         state_data = data[data.state == answer_state]
+        # Goes to the corresponding coordinates
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         guessed_states.append(answer_state)
