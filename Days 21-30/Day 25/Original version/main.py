717e58e7e3936334182192171c96e9202832ac6f
Grellheist <grellheist@tutanota.com>
2023-01-12 14:41:34 -0300

    Updated

--- Days 21-30/Day 25/Original version/main.py
+++ Days 21-30/Day 25/Original version/main.py
@@ -1,6 +1,5 @@
 import turtle
 import pandas as pd
-from titlecase import titlecase
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -10,18 +9,24 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pd.read_csv("50_states.csv")
-
-answer_state = titlecase(screen.textinput(title="Guess the State", prompt="What's"
-                                "another state's name?"))
-
 state_list = data.state.tolist()
-if answer_state in state_list:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(answer_state)
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/{len(state_list)} States Correct",
+        prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        break
+    if answer_state in state_list:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        guessed_states.append(answer_state)
 
 
 turtle.mainloop()
