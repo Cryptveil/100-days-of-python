14af923d59054e1d834fabc54338971bc3d8dd2b
Grellheist <grellheist@tutanota.com>
2023-01-12 13:09:15 -0300

    Updated

---
+++ Days 21-30/Day 25/Original version/main.py
@@ -0,0 +1,19 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+screen.setup(startx=0, starty=0)
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+def get_mouse_click_coordinate(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coordinate)
+
+
+turtle.mainloop()
