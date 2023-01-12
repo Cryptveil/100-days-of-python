5a4f85f3b65d7c7778b8e45b578342df42cb87b4
Grellheist <grellheist@tutanota.com>
2023-01-12 12:19:40 -0300

    Updated

--- Days 21-30/Day 25/main.py
+++ Days 21-30/Day 25/main.py
@@ -2,5 +2,5 @@ import pandas
 
 data = pandas.read_csv("weather_data.csv")
 temp_list = data["temp"].to_list()
-average = sum(temp_list) / len(temp_list)
-print(average)
+
+print(data["temp"].max())
