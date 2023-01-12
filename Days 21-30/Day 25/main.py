4eff9ff91bca3a7610e8fd9881a885ea3966afd5
Grellheist <grellheist@tutanota.com>
2023-01-12 12:50:12 -0300

    Updated

--- Days 21-30/Day 25/main.py
+++ Days 21-30/Day 25/main.py
@@ -1,5 +1,13 @@
 import pandas
 
 data = pandas.read_csv("squirrel_data.csv")
-fur_color = data.Primary_Fur_Color
-print(fur_color)
+fur_color = data["Primary Fur Color"]
+gray_count = fur_color.value_counts()["Gray"]
+black_count = fur_color.value_counts()["Black"]
+cinnamon_count = fur_color.value_counts()["Cinnamon"]
+dictionary = {
+        "Fur Color": ["gray", "red", "black"],
+        "Count": [gray_count, cinnamon_count, black_count]
+        }
+new_data = pandas.DataFrame(dictionary)
+new_data.to_csv("new_data.csv")
