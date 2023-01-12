10c36402ebcf8bdd72b4653f4d83670a523c1596
Grellheist <grellheist@tutanota.com>
2023-01-12 12:29:11 -0300

    Updated

--- Days 21-30/Day 25/main.py
+++ Days 21-30/Day 25/main.py
@@ -1,6 +1,3 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-temp_list = data["temp"].to_list()
-
-print(data[data.temp == data.temp.max()])
+data = pandas.read_csv("2018_Central_Park_Census_-_Squirrel_Data.csv")
