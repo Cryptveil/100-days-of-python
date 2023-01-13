18d1981f57784f295b455845df2af92fb3eecd73
Grellheist <grellheist@tutanota.com>
2023-01-13 11:24:09 -0300

    Updated

--- Days 21-30/Day 25/Original version/main.py
+++ Days 21-30/Day 25/Original version/main.py
@@ -27,10 +27,7 @@ while len(guessed_states) < 50:
 
     # Puts the missing states on another csv to review later
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_list if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("missing states.csv")
         break
