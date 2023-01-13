36264b53f86cb74bb50eef3d5b0cee556ee708df
Grellheist <grellheist@tutanota.com>
2023-01-13 12:01:00 -0300

    Updated

--- Days 21-30/Day 26/main.py
+++ Days 21-30/Day 26/main.py
@@ -7,6 +7,5 @@ new_dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 user_word = input("Enter a word: ").upper()
-input_letters = [letter for letter in user_word]
-result = [new_dictionary[letter] for letter in input_letters if letter.isalpha()]
+result = [new_dictionary[letter] for letter in user_word if letter.isalpha()]
 print(result)
