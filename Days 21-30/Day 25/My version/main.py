f7baadf135d74fda18f4dd160d36b3e833d5d1c2
Grellheist <grellheist@tutanota.com>
2023-01-13 11:26:57 -0300

    Updated

--- Days 21-30/Day 25/My version/main.py
+++ Days 21-30/Day 25/My version/main.py
@@ -24,7 +24,8 @@ while len(guessed_states) < 27:
         prompt="Qual o próximo estado?").title()
 
     if answer_state == "Sair":
-        missing_states = [state for state in state_list if state not in guessed_states]
+        missing_states = [state for state in state_list 
+                          if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("estados para aprender.csv")
         break
