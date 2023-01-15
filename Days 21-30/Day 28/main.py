ed15b7ab0e63ac0e5d890051ff91ea9d6a69d065
grellheist <grellheist@tutanota.com>
2023-01-15 13:21:15 -0300

    Updated

--- Days 21-30/Day 28/main.py
+++ Days 21-30/Day 28/main.py
@@ -13,11 +13,19 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
+def start_timer():
+    countdown(5)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+
 def countdown(count):
     canvas.itemconfig(timer_text, text=count)
     if count > 0:
         window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -29,10 +37,9 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-timer_text = canvas.create_text(100, 140, text="00:00", fill="white",
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
                                 font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
-countdown(5)
 
 # Timer label
 label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"),
@@ -40,7 +47,7 @@ label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"),
 label.grid(column=1, row=0)
 
 # Start button
-button = Button(text="Start")
+button = Button(text="Start", command=start_timer)
 button.grid(column=0, row=2)
 
 # Reset button
