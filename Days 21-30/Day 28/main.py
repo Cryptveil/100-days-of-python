c8dea375e6520ee8adbbd541d1a9d7a0bdbb0991
grellheist <grellheist@tutanota.com>
2023-01-15 13:10:49 -0300

    Updated

--- Days 21-30/Day 28/main.py
+++ Days 21-30/Day 28/main.py
@@ -21,21 +21,28 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Background and tomato image
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35,
+                                                               "bold"))
 canvas.grid(column=1, row=1)
 
-label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), highlightthickness=0, bg=YELLOW)
+# Timer label
+label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"),
+              highlightthickness=0, bg=YELLOW)
 label.grid(column=1, row=0)
 
+# Start button
 button = Button(text="Start")
 button.grid(column=0, row=2)
 
+# Reset button
 button = Button(text="Reset")
 button.grid(column=2, row=2)
 
+# Checkmark label
 label = Label(text="✔", fg=GREEN, font=(FONT_NAME, 35, "bold"), highlightthickness=0, bg=YELLOW)
 label.grid(column=1, row=3)
 
