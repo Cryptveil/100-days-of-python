5975a0fe44ec0505174145da7e1101e68168b339
grellheist <grellheist@tutanota.com>
2023-01-15 12:59:58 -0300

    Updated

--- Days 21-30/Day 28/main.py
+++ Days 21-30/Day 28/main.py
@@ -23,8 +23,8 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
-canvas.create_image(101, 112, image=tomato_image)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 window.mainloop()
