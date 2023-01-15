495e573ccf825b9263fd446875d9ac2f44e30432
grellheist <grellheist@tutanota.com>
2023-01-14 21:11:07 -0300

    Updated

--- Days 21-30/Day 27/main.py
+++ Days 21-30/Day 27/main.py
@@ -5,6 +5,27 @@ window.title("Mile to Km Converter")
 window.minsize(width=500, height=500)
 
 
+def convert_miles_to_km():
+    result = int(entry.get()) * 1.61
+    number_km.configure(text=result)
 
+
+entry = Entry(width=30)
+entry.grid(column=1, row=0)
+
+miles = Label(text="Miles")
+miles.grid(column=2, row=0)
+
+is_equal_to = Label(text="is equal to")
+is_equal_to.grid(column=0, row=1)
+
+number_km = Label(text="0")
+number_km.grid(column=1, row=1)
+
+km = Label(text="Km")
+km.grid(column=2, row=1)
+
+button = Button(text="calculate", command=convert_miles_to_km)
+button.grid(column=1, row=2)
 
 window.mainloop()
