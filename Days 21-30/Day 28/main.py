34ac16d4875d52b98976783fcdbb221bd7debeaa
grellheist <grellheist@tutanota.com>
2023-01-15 13:11:27 -0300

    Updated

--- Days 21-30/Day 28/main.py
+++ Days 21-30/Day 28/main.py
@@ -25,7 +25,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 150, text="00:00", fill="white", font=(FONT_NAME, 35,
+canvas.create_text(100, 140, text="00:00", fill="white", font=(FONT_NAME, 35,
                                                                "bold"))
 canvas.grid(column=1, row=1)
 
