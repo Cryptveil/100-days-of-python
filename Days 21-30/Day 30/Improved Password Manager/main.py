78bc77653a7f441d4e0ad694f4492c8fdeb132ea
Grellheist <grellheist@tutanota.com>
2023-01-17 11:39:36 -0300

    Updated

--- Days 21-30/Day 30/Improved Password Manager/main.py
+++ Days 21-30/Day 30/Improved Password Manager/main.py
@@ -26,7 +26,7 @@ def generate_password():
 
     random.shuffle(password_list)
 
-    password_input.delete(0, END)
+    password_input.delete(0, "end")
     password = "".join(password_list)
     password_input.insert(0, password)
     pyperclip.copy(password)
@@ -40,8 +40,10 @@ def save_password():
     email = email_input.get()
     password = password_input.get()
     new_data = {
-            "email": email,
-            "password": password,
+            website: {
+                "email": email,
+                "password": password,
+                }
             }
     if len(website) > 0 and len(password) > 0 and len(email) > 0:
         is_ok = messagebox.askokcancel(title=website, message="These are the"
@@ -50,11 +52,23 @@ def save_password():
                                        "Is this ok to save?")
 
         if is_ok:
-            with open("data.json", "w") as data_file:
-                json.dump(new_data, data_file, indent=4)
-            website_input.delete(0, END)
-            email_input.delete(0, END)
-            password_input.delete(0, END)
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)
+                    data.update(new_data)
+
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+
+            else:
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+
+            finally:
+                website_input.delete(0, "end")
+                email_input.delete(0, "end")
+                password_input.delete(0, "end")
     else:
         messagebox.showerror(title="You dumbo", message="Dont leave any of the"
                              " fields empty!!!\nI get angry when u do!!!!!!!!")
