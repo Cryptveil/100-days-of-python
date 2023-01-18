3b019531a72d9f48610bc8b94acf419fa48023b5
Grellheist <grellheist@tutanota.com>
2023-01-18 09:34:44 -0300

    Updated

--- Days 31-40/Day 31/main.py
+++ Days 31-40/Day 31/main.py
@@ -12,12 +12,13 @@ card_front = tk.PhotoImage(file="images/card_front.png")
 card_back = tk.PhotoImage(file="images/card_back.png")
 
 canvas = tk.Canvas(width=800, height=526)
-canvas.create_image(image=card_front)
+canvas.create_image(400, 263, image=card_front)
+canvas.grid(column=0, row=0, columnspan=2)
 
 correct_button = tk.Button(image=correct_image, highlightthickness=0)
 correct_button.grid(column=1, row=1)
 
 wrong_button = tk.Button(image=wrong_image, highlightthickness=0)
-wrong_button.grid(column=1, row=0)
+wrong_button.grid(column=0, row=1)
 
 window.mainloop()
