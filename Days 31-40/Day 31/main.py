cdfeaa59bc30a23203f6b253c919f5d67ac87d23
Grellheist <grellheist@tutanota.com>
2023-01-18 10:17:28 -0300

    Updated

--- Days 31-40/Day 31/main.py
+++ Days 31-40/Day 31/main.py
@@ -1,12 +1,22 @@
 import tkinter as tk
 import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_LANGUAGE = ("Arial", 40, "italic")
 FONT_WORD = ("Arial", 60, "bold")
 
 data = pd.read_csv("data/french_words.csv")
-word_dict = pd.DataFrame.to_dict(data)
+word_dict = pd.DataFrame.to_dict(data, orient="records")
+new_word = random.choice(word_dict)
+french_word = new_word["French"]
+
+
+def next_card():
+    new_word = random.choice(word_dict)
+    french_word = new_word["French"]
+    canvas.itemconfig(word, text=french_word)
+
 
 window = tk.Tk()
 window.title("Flash Card Program")
@@ -20,14 +30,16 @@ card_back = tk.PhotoImage(file="images/card_back.png")
 canvas = tk.Canvas(width=800, height=526, bg=BACKGROUND_COLOR)
 canvas.create_image(400, 263, image=card_front)
 language = canvas.create_text(400, 150, text="French", font=FONT_LANGUAGE)
-word = canvas.create_text(400, 263, text=word_dict["French"][0],
+word = canvas.create_text(400, 263, text=french_word,
                           font=FONT_WORD)
 canvas.grid(column=0, row=0, columnspan=2)
 
-correct_button = tk.Button(image=correct_image, highlightthickness=0)
+correct_button = tk.Button(image=correct_image, highlightthickness=0,
+                           command=next_card)
 correct_button.grid(column=1, row=1)
 
-wrong_button = tk.Button(image=wrong_image, highlightthickness=0)
+wrong_button = tk.Button(image=wrong_image, highlightthickness=0,
+                         command=next_card)
 wrong_button.grid(column=0, row=1)
 
 window.mainloop()
