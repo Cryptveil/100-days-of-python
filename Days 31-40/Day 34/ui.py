414fe285b880067bd9ac81297685b650bc193c87
Grellheist <grellheist@tutanota.com>
2023-01-22 00:16:48 -0300

    Updated

--- Days 31-40/Day 34/ui.py
+++ Days 31-40/Day 34/ui.py
@@ -7,5 +7,9 @@ class QuizInterface:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.
+        self.canvas = tk.Canvas(width=300, height=250, bg="white")
+        self.canvas.grid(column=0, row=1, columnspan=2)
 
         self.window.mainloop()
