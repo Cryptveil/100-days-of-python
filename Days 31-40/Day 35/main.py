f3db4d0751b484ac6ece4aec540c932013c40b87
grellheist <grellheist@tutanota.com>
2023-01-22 21:56:50 -0300

    Updated

--- Days 31-40/Day 35/main.py
+++ Days 31-40/Day 35/main.py
@@ -1,1 +1,13 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
 api_key = "0e30afb40ba29f432efe6584ffe444a7"
+
+weather_params = {
+        "lat": -16.694002,
+        "lon": -49.273652,
+        "appid": api_key,
+        }
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.
