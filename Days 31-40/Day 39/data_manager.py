04879d087e1b79a157b1607a781b402bd3ecbf19
Grellheist <grellheist@tutanota.com>
2023-01-26 14:34:09 -0300

    Updated

--- Days 31-40/Day 39/data_manager.py
+++ Days 31-40/Day 39/data_manager.py
@@ -1,3 +1,28 @@
+import requests
+
+SHEET_ENDPOINT = ""
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEET_ENDPOINT)
+        result = response.json()
+        self.destination_data = result["prices"]
+        return self.destination_data
+
+    def update_destination_data(self):
+        for city in self.destination_data:
+            new_data = {
+                    "price": {
+                        "iataCode": city["iataCode"]
+                        }
+                    }
+            response = requests.put(
+                                    url=f"{SHEET_ENDPOINT}/{city['id']}",
+                                    json=new_data
+                                    )
+            print(response.text)
