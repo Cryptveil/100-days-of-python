e57eb732c87bd9c365255c42dbdc04862cb33b75
grellheist <grellheist@tutanota.com>
2023-01-29 20:30:43 -0300

    Updated

--- Days 41-50/Day 45/main.py
+++ Days 41-50/Day 45/main.py
@@ -6,4 +6,5 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-print(soup.title)
+news = soup.find(class_="titleline")
+print(news.find(name="a").getText())
