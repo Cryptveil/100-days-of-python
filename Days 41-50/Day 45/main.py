7bf0d42a8c6768623c9dbead15ea8d32cafc7ffe
grellheist <grellheist@tutanota.com>
2023-01-29 20:35:24 -0300

    Updated

--- Days 41-50/Day 45/main.py
+++ Days 41-50/Day 45/main.py
@@ -8,3 +8,6 @@ yc_web_page = response.text
 soup = BeautifulSoup(yc_web_page, "html.parser")
 news = soup.find(class_="titleline")
 print(news.find(name="a").getText())
+print(news.find(name="a").get("href"))
+score = soup.find(class_="score")
+print(score.getText())
