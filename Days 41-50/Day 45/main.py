20641a9cda8054c34dc32644ea99c57ef5f6c5ba
grellheist <grellheist@tutanota.com>
2023-01-29 20:38:26 -0300

    Updated

--- Days 41-50/Day 45/main.py
+++ Days 41-50/Day 45/main.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://news.ycombinator.com/news")
+response = requests.get("https://news.ycombinator.com/")
+soup = BeautifulSoup(response.text, "html.parser")
 
-yc_web_page = response.text
+news = soup.find_all(class_="titleline")
+for heading in news:
+    print(heading.find(name="a").get("href"))
+    print(heading.find(name="a").getText())
 
-soup = BeautifulSoup(yc_web_page, "html.parser")
-news = soup.find(class_="titleline")
-print(news.find(name="a").getText())
-print(news.find(name="a").get("href"))
-score = soup.find(class_="score")
-print(score.getText())
+scores = soup.find_all(class_="score")
+for score in scores:
+    print(score.getText().split(" ")[0])
