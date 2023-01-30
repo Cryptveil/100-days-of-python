e2a3cb184103bf5a600c36f9c4f8e1c4f1db58db
Grellheist <grellheist@tutanota.com>
2023-01-30 15:26:04 -0300

    Updated

--- Days 41-50/Day 46/main.py
+++ Days 41-50/Day 46/main.py
@@ -1,5 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
+CLIENT_SECRET = os.environ["SPOTIPY_CLIENT_SECRET"]
+REDIRECT_URI = "http://example.com"
+SCOPE = "playlist-modify-private"
 
 date = input("Which year do you want to travel to? "
              "Type the date in the format 'YYYY-MM-DD': ")
@@ -14,3 +22,27 @@ song_names = soup.find_all(name="h3",
 
 song_list = [song.getText().strip("\n\t") for song in song_names]
 
+oauth_object = spotipy.SpotifyOAuth(client_id=CLIENT_ID,
+                                    client_secret=CLIENT_SECRET,
+                                    redirect_uri=REDIRECT_URI,
+                                    scope=SCOPE,
+                                    cache_path="token.txt")
+sp = spotipy.Spotify(auth_manager=oauth_object)
+
+user_id = sp.current_user()["id"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_list:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
