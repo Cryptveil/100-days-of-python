80263ffb10c97a84a58862186d67219381ef875e
Grellheist <grellheist@tutanota.com>
2023-01-30 14:23:26 -0300

    Updated

--- Days 41-50/Day 46/main.py
+++ Days 41-50/Day 46/main.py
@@ -1,4 +1,4 @@
-from requests_html import HTMLSession
+import requests
 from bs4 import BeautifulSoup
 
 date = input("Which year do you want to travel to? "
@@ -6,27 +6,10 @@ date = input("Which year do you want to travel to? "
 
 BILBOARD_URL = f"https://www.billboard.com/charts/hot-100/{date}"
 
+response = requests.get(BILBOARD_URL)
+soup = BeautifulSoup(response.text, "html.parser")
+song_names = soup.find_all(name="h3",
+                           id="title-of-a-story",
+                           class_="lrv-u-font-size-16")
 
-def get_web_page():
-    session = HTMLSession()
-    response = session.get(BILBOARD_URL)
-    response.html.render()
-    with open("100_songs.html", mode="w", encoding="utf-8") as file:
-        file.write(response.html)
-
-
-def read_web_file():
-    try:
-        open("100_songs.html")
-    except FileNotFoundError:
-        get_web_page()
-    finally:
-        with open("100_songs.html", mode="r", encoding="utf-8") as file:
-            data = file.read()
-            return BeautifulSoup(data, "html.parser")
-
-
-soup = read_web_file()
-song_names = soup.find_all(name="h3", id="title-of-a-story")
-
-song_list = [song.getText() for song in song_names]
+song_list = [song.getText().strip("\n\t") for song in song_names]
