37be4dd9cce28ee67c43f66ec56ba84a16195ee4
Grellheist <grellheist@tutanota.com>
2023-01-31 13:02:37 -0300

    Updated

--- Days 41-50/Day 47/main.py
+++ Days 41-50/Day 47/main.py
@@ -24,7 +24,7 @@ price = soup.find(name="span", class_="a-offscreen")
 # Since the price is in BRL, we need to change the "," to a "."
 formatted_price = float(price.getText().replace(",", ".").split("R$")[1])
 
-if formatted_price >= 400:
+if formatted_price <= 400:
     with smtplib.SMTP(SMTP_SERVER, port=PORT) as connection:
         connection.starttls()
         connection.login(user=EMAIL, password=GMAIL_TOKEN)
