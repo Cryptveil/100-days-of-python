a1221a2f471fda339ef4cbae697f6fe7bd4cef2f
Grellheist <grellheist@tutanota.com>
2023-01-31 13:02:16 -0300

    Updated

--- Days 41-50/Day 47/main.py
+++ Days 41-50/Day 47/main.py
@@ -4,7 +4,11 @@ import smtplib
 import os
 
 AMAZON_URL = "https://tinyurl.com/526but8b"
+EMAIL = "hjgameplays@gmail.com"
 GMAIL_TOKEN = os.environ["GMAIL_TOKEN"]
+SMTP_SERVER = "smtp.gmail.com"
+PORT = 587
+MSG = "Subject: Price drop!\n\nThe new price for the product is:"
 
 headers = {
         "Accept-Language": "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -21,4 +25,9 @@ price = soup.find(name="span", class_="a-offscreen")
 formatted_price = float(price.getText().replace(",", ".").split("R$")[1])
 
 if formatted_price >= 400:
-
+    with smtplib.SMTP(SMTP_SERVER, port=PORT) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=GMAIL_TOKEN)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs=EMAIL,
+                            msg=f"{MSG} R${formatted_price:.2f}")
