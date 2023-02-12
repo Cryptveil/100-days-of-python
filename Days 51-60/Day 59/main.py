2c3acaa8cd3c88d51d7e1e4a300595c91062b74c
grellheist <grellheist@tutanota.com>
2023-02-12 12:31:55 -0300

    Updated

--- Days 51-60/Day 59/main.py
+++ Days 51-60/Day 59/main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+POSTS_URL = "https://api.npoint.io/c790b4d5cab58020d391"
 
 
 @app.route("/")
 def home():
+    response = requests.get(POSTS_URL).json()
     return render_template("index.html")
 
 
