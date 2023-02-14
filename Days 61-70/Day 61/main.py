05cf17e8877cc539c996fca658fe7e16b2d85d77
Grellheist <grellheist@tutanota.com>
2023-02-14 13:20:46 -0300

    Updated

--- Days 61-70/Day 61/main.py
+++ Days 61-70/Day 61/main.py
@@ -1,3 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,8 +8,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
