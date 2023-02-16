190f8dc3034d1cc6f7ea1b85a3c8eb0e07b80ff8
grellheist <grellheist@tutanota.com>
2023-02-15 23:40:27 -0300

    Updated

--- Days 61-70/Day 62/main.py
+++ Days 61-70/Day 62/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
@@ -7,17 +7,17 @@ import csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap(app)
+Bootstrap5(app)
 
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField("Cafe Location on Google Maps (URL)", validators=[DataRequired(), URL()])
-    open = StringField("Opening Time e.g. 8AM", validators=[DataRequired()])
-    close = StringField("Closing Time e.g. 5:30PM", validators=[DataRequired()])
-    coffee_rating = SelectField("Coffee Rating", choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
-    wifi_rating = SelectField("Wifi Strength Rating", choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
-    power_rating = SelectField("Power Socket Availability", choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
+    location = StringField('Cafe Location on Goodle Maps (URL)', validators=[DataRequired(), URL()])
+    open = StringField('Opening Time e.g. 8AM', validators=[DataRequired()])
+    close = StringField('Closing Time e.g. 5:30PM')
+    coffee = SelectField('Coffee Rating', choices=[('☕'), ('☕☕'), ('☕☕☕'), ('☕☕☕☕'), ('☕☕☕☕☕')], validators=[DataRequired()])
+    wifi = SelectField('Wifi Strength Rating', choices=[('✘'), ('💪'), ('💪💪'), ('💪💪💪'), ('💪💪💪💪'), ('💪💪💪💪💪')], validators=[DataRequired()])
+    power = SelectField('Power Socket Availability', choices=[('✘'), ('🔌'), ('🔌🔌'), ('🔌🔌🔌'), ('🔌🔌🔌🔌'), ('🔌🔌🔌🔌🔌')], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -30,21 +30,25 @@ def home():
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        with open("cafe-data.csv", mode="a") as csv_file:
-            csv_file.write(f"\n{form.cafe.data},"
-                           f"{form.location.data},"
-                           f"{form.open.data},"
-                           f"{form.close.data},"
-                           f"{form.coffee_rating.data},"
-                           f"{form.wifi_rating.data},"
-                           f"{form.power_rating.data}")
-        return redirect(url_for('cafes'))
+        new_data = [
+            form.cafe.data,
+            form.location.data,
+            form.open.data,
+            form.close.data,
+            form.coffee.data,
+            form.wifi.data,
+            form.power.data
+        ]
+        with open('cafe-data.csv', 'a', encoding="utf8") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            writer.writerow(new_data)
+            return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
 
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', newline='') as csv_file:
+    with open('cafe-data.csv', newline='', encoding="utf8") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
@@ -54,4 +58,3 @@ def cafes():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
