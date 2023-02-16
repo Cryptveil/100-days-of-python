ea55c626efd9e68115611b85440aaef46e9ac1bb
Grellheist <grellheist@tutanota.com>
2023-02-16 14:52:00 -0300

    Updated

--- Days 61-70/Day 63/main.py
+++ Days 61-70/Day 63/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+# import sqlite3
 
 app = Flask(__name__)
 all_books = []
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
 # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250)\
 #        NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
