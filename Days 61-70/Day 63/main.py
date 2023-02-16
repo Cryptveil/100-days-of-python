36c1324bca270e97b565aaa1e2e5db7cc04d3991
Grellheist <grellheist@tutanota.com>
2023-02-16 15:54:02 -0300

    Updated

--- Days 61-70/Day 63/main.py
+++ Days 61-70/Day 63/main.py
@@ -1,33 +1,44 @@
-from enum import unique
 from flask import Flask, render_template, request, redirect, url_for
-# import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-all_books = []
-# db = sqlite3.connect("books-collection.db")
-# cursor = db.cursor()
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250)\
-#        NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-# db.commit()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", book_list=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_entry = {
-                "title": request.form["title"],
-                "author": request.form["author"],
-                "rating": request.form["rating"]
-                }
-        all_books.append(new_entry)
+        new_entry = Book(title=request.form["title"],
+                         author=request.form["author"],
+                         rating=request.form["rating"])
+        db.session.add(new_entry)
+        db.session.commit()
         return redirect(url_for("home"))
     return render_template("add.html")
+
+
+@app.route("/edit/<book_id>", methods=["GET", "POST"])
+def edit(book_id):
+    book = Book.query.filter_by(id=book_id).first()
+    return render_template("edit.html", book=book)
 
 
 if __name__ == "__main__":
