b1b696a20d2e4676d3d0d35cd539bd37c1d9b80f
grellheist <grellheist@tutanota.com>
2023-02-19 21:27:55 -0300

    Updated

--- Days 61-70/Day 66/main.py
+++ Days 61-70/Day 66/main.py
@@ -45,9 +45,38 @@ def get_random():
 @app.route("/all")
 def get_all():
     all_cafes = db.session.query(Cafe).all()
-    return [jsonify(cafe.to_dict()) for cafe in all_cafes]
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+
+@app.route("/search")
+def get_search():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at\
+                that location."})
+
 
 # HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
 
 # HTTP PUT/PATCH - Update Record
 
