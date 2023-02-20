59e4ec5e10666b1a507dc870bc5ec9268ab7411f
grellheist <grellheist@tutanota.com>
2023-02-20 01:01:35 -0300

    Updated

--- Days 61-70/Day 63/main.py
+++ Days 61-70/Day 63/main.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Book(db.Model):
+class Book(db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     author = db.Column(db.String(250), nullable=False)
