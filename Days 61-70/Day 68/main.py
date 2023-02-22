d7f871c0efcda2ffc546072e02c154833c5d5052
grellheist <grellheist@tutanota.com>
2023-02-22 00:23:53 -0300

    Updated

--- Days 61-70/Day 68/main.py
+++ Days 61-70/Day 68/main.py
@@ -29,10 +29,15 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
         new_user = User(
                 email=request.form.get("email"),
                 name=request.form.get("name"),
-                password=request.form.get("password")
+                password=hash_and_salted_password
                 )
         db.session.add(new_user)
         db.session.commit()
@@ -57,7 +62,7 @@ def logout():
 
 @app.route('/download')
 def download():
-    pass
+    return send_from_directory("static", path="files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
